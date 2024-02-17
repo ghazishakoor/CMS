fdb0631d97b586a16cacb82e66b3218f4e008e91
Ghazi Shakoor <ghazishakoor@gmail.com>
2024-02-16 20:12:17 -0500

    second commit after student_form correction enctype=multipart/form-data

--- cmsmain/urls.py
+++ cmsmain/urls.py
@@ -2,10 +2,13 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-
+from . import views
 
 urlpatterns = [
-    
+    path('student_list/', views.StudentListView.as_view(), name='student_list'),
+    path('student_detail/<int:pk>/', views.StudentDetailView.as_view(), name='student_detail'),
+    path('student_create/', views.StudentCreateView.as_view(), name='student_create'),
+    path('success/', views.my_success_view, name='success'),
 ]
 
 if settings.DEBUG:
--- cmsmain/views.py
+++ cmsmain/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
+
+from .forms import *
+from .models import *
 
 # Create your views here.
+
+
+class StudentListView(ListView):
+    model = Student
+
+
+class StudentDetailView(DetailView):
+    model = Student
+    template_name = 'cmsmain/student_detail.html'
+    
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        student = self.get_object()
+        context["subjects"] = student.subjects.all()
+        return context
+
+class StudentCreateView(CreateView):
+    model = Student
+    fields = '__all__'
+    success_url = '/success/'
+
+def my_success_view(request):
+    return render(request, 'cmsmain/student_success.html')
